a0a6e713951b799a81c5d8460faa82c349f0f0e6
Jacob Iglesias <jacob.ds.iglesias.5@gmail.com>
2020-10-09 22:20:53 -0400

    7-2

--- SimpleCalculator/Form1.cs
+++ SimpleCalculator/Form1.cs
@@ -17,6 +17,78 @@ namespace SimpleCalculator
             InitializeComponent();
         }
 
+        public bool IsPresent(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool IsDecimal(TextBox textBox, string name)
+        {
+            decimal number = 0m;
+            if (Decimal.TryParse(textBox.Text, out number)) { return true; }
+            else
+            {
+                MessageBox.Show(name + " must be a decimal value.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+
+        public bool IsWithinRange(TextBox textBox, string name, decimal min, decimal max)
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if (number < min || number > max)
+            {
+                MessageBox.Show(name + " must be between " +
+                    Convert.ToString(min) + " and " + Convert.ToString(max) + ".",
+
+                    "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            else { return true; }
+        }
+
+        public bool IsOperator(TextBox textBox)
+        {
+            string[] ops = { "+", "-", "*", "/" };
+            if (ops.Contains(textBox.Text)) { return true; }
+            else 
+            {
+                MessageBox.Show("Invalid operator; use +, -, *, /", "Entry Error");
+                textBox.Focus();
+
+                return false;
+            }
+        }
+
+        public bool IsValidData()
+        {
+            if (
+                IsPresent(tbOp1, "Operand 1") &&
+                IsPresent(tbOp2, "Operand 2") &&
+                IsPresent(tbOper, "Operator") &&
+
+                IsDecimal(tbOp1, "Operand 1") &&
+                IsDecimal(tbOp2, "Operand 2") &&
+                IsOperator(tbOper) &&
+
+                IsWithinRange(tbOp1, "Operand 1", 0m, 1000000m) &&
+                IsWithinRange(tbOp2, "Operand 2", 0m, 1000000m)
+
+            ) { return true; }
+            else { return false; }
+        }
+
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -27,13 +99,13 @@ namespace SimpleCalculator
         private void btnCalc_Click(object sender, EventArgs e)
         {
             // Declare output of try block for later use
-            Decimal result;
+            decimal result;
 
             //start error checking
             try
             {
                 // get vars and store as Decimal
-                Decimal op1, op2;
+                decimal op1, op2;
                 op1 = Convert.ToDecimal(tbOp1.Text);
                 op2 = Convert.ToDecimal(tbOp2.Text);
 
@@ -61,55 +133,17 @@ namespace SimpleCalculator
                         throw new InvalidOperationException(); // operator is null
                 }
             }
-            catch (FormatException)
-            {
-                MessageBox.Show(
-                   "FormatExcpetion: Non-numberic input for operator." + "\n",
-
-                   "FormatExcpetion Error");
-
-                tbResult.Text = "";
-                return;
-            }
-            catch (OverflowException)
-            {
-                MessageBox.Show(
-                   "OverflowException: Numeric output too large." + "\n",
-
-                   "OverflowException Error");
-
-                tbResult.Text = "";
-                return;
-            }
-            catch (DivideByZeroException)
-            {
-                MessageBox.Show(
-                   "DivideByZeroException: Cannot divide by 0." + "\n",
-
-                   "DivideByZeroException Error");
-
-                tbResult.Text = "";
-                return;
-            }
-            catch (InvalidOperationException)
-            {
-                MessageBox.Show(
-                   "InvalidOperationException: Operator missing or invalid. (Use +, -, *, /)" + "\n",
-
-                   "InvalidOperationException Error");
-
-                tbResult.Text = "";
-                return;
-            }
             // Catch all others and show stack
             catch (Exception ex)
             {
-                MessageBox.Show(
-                    ex.ToString(),
+                if (IsValidData()) {
+                    MessageBox.Show(
+                        ex.ToString(),
 
-                    "Error");
+                        "Error");
 
-                tbResult.Text = "";
+                    tbResult.Text = "";
+                }
                 return;
             }
 
